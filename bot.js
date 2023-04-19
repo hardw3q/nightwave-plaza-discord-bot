372491ff288c15e3c65412f0f4b03f9517b09946
hardw3q <sob.dod@gmail.com>
2023-04-19 16:10:17 +0300

    init running bot

---
+++ bot.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('ffmpeg-static');
+const opus = require('@discordjs/opus');
+const request = require('request')
+const { joinVoiceChannel, createAudioResource, createAudioPlayer} = require('@discordjs/voice');
+const {cleanContent} = require("discord.js");
+
+
+
+const client = new Discord.Client({intents: 33409});
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on(Discord.Events.MessageCreate, async message => {
+    console.log(message.content)
+    if (message.content === '!play') {
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) {
+            return message.reply('Please join a voice channel first!');
+        }
+        const connection = await joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: voiceChannel.guildId,
+            adapterCreator: voiceChannel.guild.voiceAdapterCreator
+        });
+
+        const resource = createAudioResource('https://radio.plaza.one/mp3');
+
+        const player = createAudioPlayer();
+
+        player.play(resource);
+
+        connection.subscribe(player)
+        await message.reply('Now playing in ' + voiceChannel.name);
+    }
+
+    if (message.content === '!stop') {
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) {
+            return message.reply('Please join a voice channel first!');
+        }
+
+        voiceChannel.leave();
+
+        message.reply('Stopping playback in ' + voiceChannel.name);
+    }
+});
+
+client.login('MTA5NjEyMzYzOTUxNjExMDg1OA.GWfAuu.0jdIJmmMtDrdmoSB6dljlYDa_C1x4Kj9wqgpNI');
